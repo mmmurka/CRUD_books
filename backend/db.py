0a942d714e3a5a094af7a401dd776dd2def3ca08
mmmurka <murenkodan2005@gmail.com>
2024-07-07 14:17:11 +0300

    added db.py

---
+++ backend/db.py
@@ -0,0 +1,27 @@
+import sqlite3
+from sqlite3 import Connection
+
+DATABASE_NAME = "books.db"
+
+
+def get_db_connection() -> Connection:
+    connection = sqlite3.connect(DATABASE_NAME)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def create_books_table():
+    with get_db_connection() as conn:
+        conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS books (
+                id INTEGER PRIMARY KEY,
+                title TEXT NOT NULL,
+                author TEXT NOT NULL,
+                published_date TEXT NOT NULL,
+                isbn TEXT NOT NULL,
+                pages INTEGER NOT NULL
+            )
+        """
+        )
+        conn.commit()
